daaf658d1d50629a3d9559139623e049dd31ddf7
Martin Drazkovec <536686@mail.muni.cz>
2025-03-12 14:53:58 +0100

    connect fast api to backend

--- scripts/embedding_helpers.py
+++ scripts/embedding_helpers.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scripts.helpers import nice_dict_print
+from scripts.helpers import load_courses, nice_dict_print
 
 def similarity(vector1, vector2):
     cosine_similarity = np.dot(vector1, vector2) / (np.linalg.norm(vector1) * np.linalg.norm(vector2))
@@ -27,8 +27,8 @@ def courses_codes_to_indices(courses_codes, ctoi):
 def recommend_based_on_liked_disliked(liked, disliked, all_embeds, ctoi, n):
     liked_indices = courses_codes_to_indices(liked, ctoi)
     disliked_indices = courses_codes_to_indices(disliked, ctoi)
-    all_embeds = np.array(all_embeds)
     liked_embeds = all_embeds[liked_indices]
     disliked_embeds = all_embeds[disliked_indices]
     combined_embed = add_embeddings(liked_embeds, disliked_embeds)
-    return [(i, sim) for i, _, sim in sort_by_similarity(combined_embed, all_embeds)][:n]
+    res = [(i, _, sim) for i, _, sim in sort_by_similarity(combined_embed, all_embeds)][:n]
+    return [i for i, _, _ in res], [sim for _, _, sim in res]
--- web/backend/main.py
+++ web/backend/main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from scripts.embedding_helpers import recommend_based_on_liked_disliked
+from typing import List
+from pydantic import BaseModel
+import numpy as np
+from scripts.helpers import load_courses
+
+class Item(BaseModel):
+    recommended_courses: List[dict]
 
 app = FastAPI()
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+courses, ctoi = load_courses("data/generated")
+all_embeds = np.load(f"data/embeddings/embeds.npy", allow_pickle=True)
+
+=======
+>>>>>>> 6ae6a5a5b44f68c8f7da0c90d9c97af4ea698c65
+>>>>>>> Stashed changes
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/recommendations", response_model=Item)
+async def recommendations(liked: List[str], disliked: List[str], n: int) -> Item:
+    recommended_courses_indices, similarities = recommend_based_on_liked_disliked(liked, disliked, all_embeds, ctoi, n)
+    recommended_courses = [courses[i] for i in recommended_courses_indices]
+    for i, recommended_course in enumerate(recommended_courses):
+        recommended_course["similarity"] = similarities[i]
+    return {"recommended_courses": recommended_courses}
