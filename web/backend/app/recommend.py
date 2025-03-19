838b1f69c4a905f6579f5cad6d01cc8c4376078e
Martin Drazkovec <536686@mail.muni.cz>
2025-03-19 20:53:13 +0100

    update backend with new way of adding pos, neg vectors

--- web/backend/app/recommend.py
+++ web/backend/app/recommend.py
@@ -10,12 +10,8 @@ def compute_similarity(vector1, vector2):
     return cosine_similarity
 
 def add_embeddings(pos_vectors, neg_vectors):
-    if len(neg_vectors) == 0:
-        return np.mean(pos_vectors, axis=0)
-    if len(pos_vectors) == 0:
-        return -np.mean(neg_vectors, axis=0)
-    # TODO: do we want to weight positive and negative vectors the same irrespective of the number of vectors?
-    return np.mean(pos_vectors, axis=0) - np.mean(neg_vectors, axis=0)
+    all_vectors = np.concatenate([pos_vectors, -neg_vectors], axis=0)
+    return np.mean(all_vectors, axis=0)
 
 def sort_by_similarity(target, candidates):
     candidates = [(i, c, compute_similarity(target, c)) for i, c in enumerate(candidates)]
