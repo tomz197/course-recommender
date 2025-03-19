8442b3b52e403bd4a01853423c7d6e3d4979785a
Martin Drazkovec <536686@mail.muni.cz>
2025-03-19 20:56:35 +0100

    change which embeds to use in backend

--- web/backend/main.py
+++ web/backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
 )
 
 courseClient = CourseClient("./assets/courses/")
-all_embeds: npt.NDArray = np.load(f"./assets/embeds.npy", allow_pickle=True)
+all_embeds: npt.NDArray = np.load(f"./assets/embeds_from_catalogue_reduced.npy", allow_pickle=True)
 
 @app.post("/recommendations", response_model=RecommendationResponse)
 async def recommendations(liked: List[str], disliked: List[str], n: int) -> RecommendationResponse:
