e329c2f2116c87b0794b6f25a6c1d65c71e67da2
Martin Drazkovec <536686@mail.muni.cz>
2025-03-20 11:35:28 +0100

    new recommendation method from embeds

--- scripts/embedding_helpers.py
+++ scripts/embedding_helpers.py
@@ -7,23 +7,39 @@ def similarity(vector1, vector2):
 def embed(models, content):
     return models.embed_content(model="text-embedding-004", contents=content)
 
-def add_embeddings(pos_vectors, neg_vectors):
+def combine_pos_neg_embeds(pos_vectors, neg_vectors):
     all_vectors = np.concatenate([pos_vectors, -neg_vectors], axis=0)
     return np.mean(all_vectors, axis=0)
 
-def sort_by_similarity(target, candidates):
-    candidates = [(i, c, similarity(target, c)) for i, c in enumerate(candidates)]
+def sort_by_similarity(liked_embeds, disliked_embeds, candidate_embeds, algorithm):
+    if algorithm == "new":
+        candidates = [(i, c, score_by_adding_scores(liked_embeds, disliked_embeds, c)) for i, c in enumerate(candidate_embeds)]
+    else:
+        candidates = [(i, c, score_with_one_interest_embed(liked_embeds, disliked_embeds, c)) for i, c in enumerate(candidate_embeds)]
     candidates.sort(key=lambda x: x[2], reverse=True)
     return candidates
 
 def courses_codes_to_indices(courses_codes, ctoi):
     return [ctoi[code] for code in courses_codes]
 
-def recommend_based_on_liked_disliked(liked, disliked, all_embeds, ctoi, n):
+def recommend_based_on_liked_disliked(liked, disliked, all_embeds, ctoi, n, algorithm="old"):
     liked_indices = courses_codes_to_indices(liked, ctoi)
     disliked_indices = courses_codes_to_indices(disliked, ctoi)
     liked_embeds = all_embeds[liked_indices]
     disliked_embeds = all_embeds[disliked_indices]
-    combined_embed = add_embeddings(liked_embeds, disliked_embeds)
-    res = [(i, _, sim) for i, _, sim in sort_by_similarity(combined_embed, all_embeds)][:n]
+    res = [(i, _, sim) for i, _, sim in sort_by_similarity(liked_embeds, disliked_embeds, all_embeds, algorithm)][:n]
     return [i for i, _, _ in res], [sim for _, _, sim in res]
+
+def score_with_one_interest_embed(liked_embeds, disliked_embeds, candidate_embed):
+    combined_embed = combine_pos_neg_embeds(liked_embeds, disliked_embeds)
+    return similarity(combined_embed, candidate_embed)
+
+def score_by_adding_scores(liked_embeds, disliked_embeds, candidate_embed):
+    score = 0
+    for liked_embed in liked_embeds:
+        score += similarity(liked_embed, candidate_embed) ** 2
+    
+    for disliked_embed in disliked_embeds:
+        score -= 0.5 * similarity(disliked_embed, candidate_embed) ** 2
+
+    return score
