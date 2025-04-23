04d06502470bf7ffe00e6e5bba66648d64b148a0
Marek <mareklicko448@gmail.com>
2025-04-23 15:34:06 +0200

    Added TF-IDF

---
+++ web/backend/app/recommend/tfidf.py
@@ -0,0 +1,58 @@
+import scipy.sparse as sp
+from typing import List, Tuple
+from app.courses import CourseClient
+from app.types import CourseWithId
+import numpy as np
+import pickle
+
+loaded_sparse_matrix = sp.load_npz("./assets/intersects_tfidf.npz")
+kwd_intersects = loaded_sparse_matrix.toarray().astype(np.float32)
+course_indices = pickle.load(open("./assets/course_indices_tfidf.pkl", "rb"))
+
+print(kwd_intersects.shape)
+
+def find_top_courses(
+    idx_liked: List[int], idx_disliked: List[int], matrix: np.ndarray
+) -> List[Tuple[int, float]]:
+    liked_scores = matrix[idx_liked]
+    disliked_scores = matrix[idx_disliked]
+
+    summed = liked_scores.sum(axis=0) - disliked_scores.sum(axis=0)
+
+    print(summed.shape)
+
+    course_scores = [(i, score) for i, score in enumerate(summed)]
+    course_scores.sort(key=lambda x: x[1], reverse=True)
+
+    return course_scores
+
+
+def recommend_courses_keywords_tfidf(
+    liked: List[str],
+    disliked: List[str],
+    skipped: List[str],
+    courseClient: CourseClient,
+    n: int,
+) -> List[CourseWithId]:
+    ctoi = {code: idx for idx, code in course_indices.items()}
+    liked_ids = [ctoi[code] for code in liked]
+    disliked_ids = [ctoi[code] for code in disliked]
+    skipped_ids = [ctoi[code] for code in skipped]
+
+    top_courses = find_top_courses(liked_ids, disliked_ids, kwd_intersects)
+
+    res = []
+    for idx, _ in top_courses:
+        if idx in liked_ids or idx in disliked_ids or idx in skipped_ids:
+            continue
+        # course = courseClient.get_course_by_id(idx)
+        print(idx)
+        course_code = course_indices[idx]
+        print(course_code)
+        course = courseClient.get_course_by_code(course_code)
+        if course is not None:
+            res.append(course)
+        if len(res) == n:
+            break
+
+    return res
--- web/backend/main.py
+++ web/backend/main.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
+
 load_dotenv()
 import os
 
-from fastapi import FastAPI 
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
@@ -12,8 +13,14 @@ import numpy as np
 from app.recommend.embeddings import recommend_courses, recommend_average
 from app.recommend.keywords import recommend_courses_keywords
 from app.recommend.baseline import recommend_courses_baseline
+from app.recommend.tfidf import recommend_courses_keywords_tfidf
 from app.courses import CourseClient
-from app.types import CourseWithId, RecommendationFeedbackLog, UserFeedbackLog, RecommendationResponse
+from app.types import (
+    CourseWithId,
+    RecommendationFeedbackLog,
+    UserFeedbackLog,
+    RecommendationResponse,
+)
 from app.db.mongo import MongoDBLogger
 
 
@@ -27,30 +34,51 @@ app.add_middleware(
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
 courseClient = CourseClient(os.path.join("assets", "courses"))
-all_embeds: npt.NDArray = np.load(os.path.join("assets", "embeddings_tomas_03.npy"), allow_pickle=True)
+all_embeds: npt.NDArray = np.load(
+    os.path.join("assets", "embeddings_tomas_03.npy"), allow_pickle=True
+)
 db = MongoDBLogger()
 
 
 @app.post("/recommendations", response_model=RecommendationResponse)
-async def recommendations(liked: List[str], disliked: List[str], skipped: List[str], n: int, model: str = "average") -> RecommendationResponse:
+async def recommendations(
+    liked: List[str],
+    disliked: List[str],
+    skipped: List[str],
+    n: int,
+    model: str = "average",
+) -> RecommendationResponse:
     recommended_courses = None
     if model == "embeddings_v1":
-        recommended_courses = recommend_courses(liked, disliked, skipped, all_embeds, courseClient, n)
+        recommended_courses = recommend_courses(
+            liked, disliked, skipped, all_embeds, courseClient, n
+        )
     elif model == "average":
-        recommended_courses = recommend_average(liked, disliked, skipped, all_embeds, courseClient, n)
+        recommended_courses = recommend_average(
+            liked, disliked, skipped, all_embeds, courseClient, n
+        )
     elif model == "keywords":
-        recommended_courses = recommend_courses_keywords(liked, disliked, skipped, courseClient, n)
+        recommended_courses = recommend_courses_keywords(
+            liked, disliked, skipped, courseClient, n
+        )
     elif model == "baseline":
-        recommended_courses = recommend_courses_baseline(liked, disliked, skipped, courseClient, n)
+        recommended_courses = recommend_courses_baseline(
+            liked, disliked, skipped, courseClient, n
+        )
+    elif model == "keywords_tfidf":
+        recommended_courses = recommend_courses_keywords_tfidf(
+            liked, disliked, skipped, courseClient, n
+        )
 
     if recommended_courses is None:
         raise ValueError("Model not found")
     return RecommendationResponse(recommended_courses=recommended_courses)
+
 
 @app.get("/course/{course_id}", response_model=CourseWithId)
 async def course(course_id: str) -> CourseWithId:
@@ -59,9 +87,11 @@ async def course(course_id: str) -> CourseWithId:
         raise ValueError("Course not found")
     return found
 
+
 @app.get("/models", response_model=List[str])
 async def models() -> List[str]:
-    return ["average", "keywords", "baseline", "embeddings_v1"]
+    return ["average", "keywords", "baseline", "embeddings_v1", "keywords_tfidf"]
+
 
 @app.get("/health")
 async def health() -> dict:
@@ -70,7 +100,15 @@ async def health() -> dict:
 
 @app.post("/log_recommendation_feedback")
 async def log_recommendation_feedback(log: RecommendationFeedbackLog) -> None:
-    db.log_recommendation_feedback(log.liked, log.disliked, log.skipped, log.course, log.action, log.user_id, log.model)
+    db.log_recommendation_feedback(
+        log.liked,
+        log.disliked,
+        log.skipped,
+        log.course,
+        log.action,
+        log.user_id,
+        log.model,
+    )
 
 
 @app.post("/log_user_feedback")
